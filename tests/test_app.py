0a6ef3cb4a1e86e4f8cabbfdbeb04b8137cd2c14
Harsh Devisha <harshdevisha91@gmail.com>
2024-04-21 01:56:01 -0700

    added login tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,25 @@
 from services.user_service import logged_in
+from flask import request
+from bs4 import BeautifulSoup
 
 
 def test_login_view(client) -> None:
     response = client.get("/login")
-    with client.application.test_request_context():
-        if logged_in():
-            assert response.status_code == 303
-        else:
-            assert response.status_code == 200
+    assert "Email:" in response.text
+    assert "Password:" in response.text
+    assert response.status_code in [200, 302]
+
+
+def test_login_api(client) -> None:
+    response = client.get('/login')
+    soup = BeautifulSoup(response.data, "html.parser")
+    csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
+    form_data = {
+        'email': 'alice@example.com',
+        'password': '123456',
+        'csrf_token': csrf_token
+    }
+    assert client.get_cookie('auth_token') == None
+    response = client.post("/login",  data=form_data)
+    assert client.get_cookie('auth_token') != None
+    assert response.status_code == 303
