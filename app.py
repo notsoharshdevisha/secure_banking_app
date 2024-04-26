326a901d72050ed9da38603e2c43b5f0fe894892
Harsh Devisha <harshdevisha91@gmail.com>
2024-04-25 17:18:02 -0700

    reconfigured rate limiter

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from app_factory import create_app
-from flask import redirect, request
-from services.user_service import logged_in
 from middlewares import check_auth
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 app = create_app()
+
+Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per hour", "2 per second"],
+    storage_uri="memory://",
+)
 
 check_auth(app)
 
--- app_factory.py
+++ app_factory.py
@@ -1,20 +1,11 @@
 from flask import Flask
 from routes import main_bp
 from config import get_config
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 
 def create_app():
     # Create the Flask app instance
     app = Flask(__name__)
-
-    Limiter(
-        get_remote_address,
-        app=app,
-        default_limits=["200 per hour", "2 per second"],
-        storage_uri="memory://",
-    )
 
     config = get_config()
 
